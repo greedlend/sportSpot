9f80c30e3ac9b534af93c707fdf027dfe020e608
greedlend <andy010833@gmail.com>
2021-02-24 17:50:58 +0800

    Update spot.js

--- routes/spot.js
+++ routes/spot.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
 /* GET specific spot. */
 router.get('/spot/{id}', function(req, res, next) {
   res.send('respond with a resource');
